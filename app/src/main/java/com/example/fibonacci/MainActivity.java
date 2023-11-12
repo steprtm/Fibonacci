4ab9c3597d830d18b4cc80cecd83f51f7736e690
steprtm <kurnia16j@gmail.com>
2023-11-12 20:12:28 +0700

    Menambahkan Tombol Max Number

--- app/src/main/java/com/example/fibonacci/MainActivity.java
+++ app/src/main/java/com/example/fibonacci/MainActivity.java
@@ -1,27 +1,34 @@
 package com.example.fibonacci;
 
+import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
 
-
 public class MainActivity extends AppCompatActivity {
     private static final int NUM_COLORS = 7;
+    private int previousFibonacci = 1;
 
     private int currentFibonacci = 0;
+    private int maxFibonacciValue = 100; // Default max value
     private TextView textView;
     private Button showFibonacciButton;
     private Button resetButton;
-    private int previousFibonacci = 1;
+    private Button showToastButton;
+    private Button enterMaxButton;
+
     private void changeTextColor() {
         Resources res = getResources();
         TypedArray colors = res.obtainTypedArray(R.array.fibonacci_colors);
@@ -40,23 +47,33 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.fibonacciNumberTextView);
         showFibonacciButton = findViewById(R.id.showFibonacciButton);
         resetButton = findViewById(R.id.resetButton);
+        showToastButton = findViewById(R.id.showToastButton);
+        enterMaxButton = findViewById(R.id.enterMaxButton);
 
+        enterMaxButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showMaxInputDialog();
+            }
+        });
 
-        Button showToastButton = findViewById(R.id.showToastButton);
-
-    showFibonacciButton.setOnClickListener(new View.OnClickListener() {
+        showFibonacciButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int nextFibonacci = currentFibonacci + previousFibonacci;
-                textView.setText(String.valueOf(nextFibonacci));
-                changeTextColor();
-                previousFibonacci = currentFibonacci;
-                currentFibonacci = nextFibonacci;
 
+                if (maxFibonacciValue == 0 || nextFibonacci <= maxFibonacciValue) {
+                    textView.setText(String.valueOf(nextFibonacci));
+                    changeTextColor();
+                    previousFibonacci = currentFibonacci;
+                    currentFibonacci = nextFibonacci;
+                } else {
+                    Toast.makeText(MainActivity.this, "Reached Max Fibonacci Value", Toast.LENGTH_SHORT).show();
+                }
             }
         });
-    showToastButton = findViewById(R.id.showToastButton);
-    showToastButton.setOnClickListener(new View.OnClickListener() {
+
+        showToastButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String toastMessage = "Fibonacci Number: " + currentFibonacci;
@@ -66,7 +83,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         resetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -75,5 +91,41 @@ public class MainActivity extends AppCompatActivity {
                 textView.setText("0");
             }
         });
+    }
+
+    private void showMaxInputDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Enter Max Number, 0 = No Limit");
+
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_NUMBER);
+        builder.setView(input);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                try {
+                    int newMax = Integer.parseInt(input.getText().toString());
+
+                    if (newMax >= 0) {
+                        maxFibonacciValue = newMax;
+                        // No toast message for unlimited Fibonacci sequence
+                    } else {
+                        Toast.makeText(MainActivity.this, "Enter a valid non-negative number", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (NumberFormatException e) {
+                    Toast.makeText(MainActivity.this, "Invalid input", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
     }
 }
